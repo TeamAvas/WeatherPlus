a037d2ae06ec31a0b05d7f26b22dacefbb6a6482
Song ki ho <44698603+GodVas@users.noreply.github.com>
2021-05-19 18:34:17 +0900

    Update WeatherCommand.php

--- src/skh6075/weatherplus/command/WeatherCommand.php
+++ src/skh6075/weatherplus/command/WeatherCommand.php
@@ -5,10 +5,8 @@ namespace skh6075\weatherplus\command;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
-use pocketmine\utils\TextFormat;
-use skh6075\weatherplus\session\Weather;
-use skh6075\weatherplus\session\WeatherIds;
-use skh6075\weatherplus\session\WeatherSession;
+use skh6075\weatherplus\weather\Weather;
+use skh6075\weatherplus\weather\WeatherIds;
 use skh6075\weatherplus\WeatherPlus;
 
 final class WeatherCommand extends Command{
@@ -22,12 +20,7 @@ final class WeatherCommand extends Command{
     }
 
     public function execute(CommandSender $player, string $label, array $args): bool{
-        if (!$player instanceof Player) {
-            $player->sendMessage(TextFormat::RED . "Please, this command use only in-game");
-            return false;
-        }
-
-        if (!$this->testPermission($player)) {
+        if (!$player instanceof Player or !$this->testPermission($player)) {
             return false;
         }
 
@@ -37,12 +30,11 @@ final class WeatherCommand extends Command{
             return false;
         }
 
-        $session = WeatherSession::getWeatherSession($world = $player->getWorld());
-        if (!$session instanceof Weather) {
-            WeatherSession::registerWeatherSession($world);
+        if (!($weather = $this->plugin->getWeatherByWorld($player->getWorld())) instanceof Weather) {
+            $this->plugin->registerWorld($player->getWorld());
         }
 
-        $session->setWeather(WeatherIds::getNameByWeatherId($type));
+        $weather->setWeather(WeatherIds::getWeathers()[$type] ?? WeatherIds::SUNNY, 12000);
         $player->sendMessage("Change World Weather.");
         return true;
     }
